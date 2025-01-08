5498fa9036c23a015303523285fdb8672b44ddfa
phsarafian <philippe.sarafian@citalid.com>
2025-01-08 14:47:52 +0100

    feat(#1): create spring project

---
+++ src/test/java/net/efrei/springrag/SpringragApplicationTests.java
@@ -0,0 +1,13 @@
+package net.efrei.springrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringragApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
